2dc831dcab1e7e4c1079f3daeb3faee64efc6211
khamal45 <90587995+khamal45@users.noreply.github.com>
2026-06-30 13:55:25 +0700

    Fix kedi infolist

--- app/Filament/Resources/ProduksiKedis/Schemas/ProduksiKediInfolist.php
+++ app/Filament/Resources/ProduksiKedis/Schemas/ProduksiKediInfolist.php
@@ -12,11 +12,18 @@ class ProduksiKediInfolist
         return $schema
             ->components([
                 TextEntry::make('tanggal')
-                    ->date(),
+                    ->date()
+                    ->label('Tanggal Masuk'),
                 TextEntry::make('status'),
+                TextEntry::make('rencana_bongkar')
+                    ->date(),
+                TextEntry::make('tanggal_bongkar')
+                    ->date(),
 
                 TextEntry::make('mesin.nama_mesin')
                     ->label('Mesin Kedi'),
+                TextEntry::make('kendala'),
+
                 // TextEntry::make('created_at')
                 //     ->dateTime(),
                 // TextEntry::make('updated_at')
