23b6d2b54ad267bbeaa77b6624c62b5fd47fe8d9
FarisEka <fariseka3@gmail.com>
2026-07-06 09:26:43 +0700

    new menu stok and log platform mth page and blade

---
+++ app/Filament/Pages/StokPlatformMth.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Models\StokPlatformMth as StokPlatformMthModel;
+use App\Models\JenisKayu;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+use Filament\Pages\Page;
+use UnitEnum;
+
+class StokPlatformMth extends Page
+{
+    use HasPageShield;
+
+    protected string $view = 'filament.pages.stok-platform-mth';
+
+    protected static ?string $navigationLabel = 'Stok Platform MTH';
+    protected static string|UnitEnum|null $navigationGroup = 'Stok';
+    protected static ?string $title          = 'Stok Platform MTH';
+    protected static ?int    $navigationSort = 13;
+
+    // State untuk filtering di UI Blade
+    public string $filterJenisKayu = '';
+    public string $filterTebal     = '';
+    public string $filterKw        = '';
+
+    public function getSummariesProperty()
+    {
+        return StokPlatformMthModel::with(['jenisKayu', 'lastLog'])
+            ->when($this->filterJenisKayu, fn($q) => $q->where('id_jenis_kayu', $this->filterJenisKayu))
+            ->when($this->filterTebal,     fn($q) => $q->where('tebal',     $this->filterTebal))
+            ->when($this->filterKw,        fn($q) => $q->where('kw_grade', $this->filterKw))
+            ->where('stok_lembar', '>', 0)
+            ->get();
+    }
+
+    public function getGroupedSummariesProperty()
+    {
+        return $this->summaries->groupBy('tebal')->sortKeys();
+    }
+
+    public function getKwListProperty()
+    {
+        return StokPlatformMthModel::where('stok_lembar', '>', 0)->distinct()->pluck('kw_grade');
+    }
+
+    public function getTebalListProperty()
+    {
+        return StokPlatformMthModel::where('stok_lembar', '>', 0)->distinct()->pluck('tebal');
+    }
+
+    public function getTotalNilaiStokProperty(): float
+    {
+        return (float) StokPlatformMthModel::where('stok_lembar', '>', 0)
+            ->when($this->filterJenisKayu, fn($q) => $q->where('id_jenis_kayu', $this->filterJenisKayu))
+            ->sum('nilai_stok');
+    }
+
+    public function getTotalLembarProperty(): int
+    {
+        return (int) StokPlatformMthModel::where('stok_lembar', '>', 0)
+            ->when($this->filterJenisKayu, fn($q) => $q->where('id_jenis_kayu', $this->filterJenisKayu))
+            ->sum('stok_lembar');
+    }
+}
